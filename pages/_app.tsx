7de2fef55662eabba6550cc232c4bc558a9bc0ad
JulianePires <mpiresjuliane@outlook.com>
2021-07-13 23:36:33 -0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+export default MyApp;
